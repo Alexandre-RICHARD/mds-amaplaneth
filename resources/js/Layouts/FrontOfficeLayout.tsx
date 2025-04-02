c51570aa3ccaa7afba029d5adb1b62ef308f8678
Mahora Grolleau <hiphopfraise@gmail.com>
2025-04-02 16:37:26 +0200

    Feat : home page v1.0
    Fix #1

--- resources/js/Layouts/FrontOfficeLayout.tsx
+++ resources/js/Layouts/FrontOfficeLayout.tsx
@@ -1,20 +1,53 @@
 import ApplicationLogo from '@/Components/ApplicationLogo';
 import NavLink from '@/Components/NavLink';
 import { Link } from '@inertiajs/react';
-import { PropsWithChildren, ReactNode } from 'react';
+import { PropsWithChildren, ReactNode, useState } from 'react';
 
 export default function FrontOffice({
     header,
     image,
     children,
 }: PropsWithChildren<{ header?: ReactNode; image?: string }>) {
+    const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+    const toggleHamburger = () => {
+        setHamburgerOpen(!hamburgerOpen);
+    };
     return (
         <div>
             <header className="sticky top-0 z-20 flex justify-between rounded-b-xl bg-white p-5 uppercase">
                 <Link href="/">
                     <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                 </Link>
-                <nav className="flex w-[50%] justify-around">
+                <button
+                    data-collapse-toggle="navbar-default"
+                    type="button"
+                    className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden"
+                    aria-controls="navbar-default"
+                    aria-expanded="false"
+                    onClick={toggleHamburger}
+                >
+                    <span className="sr-only">Open main menu</span>
+                    <svg
+                        className="h-5 w-5"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 17 14"
+                    >
+                        <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M1 1h15M1 7h15M1 13h15"
+                        />
+                    </svg>
+                </button>
+                <nav
+                    id="navbar-default"
+                    className="flex justify-around max-sm:hidden"
+                >
                     <NavLink
                         href={route('dashboard')}
                         active={route().current('contract')}
@@ -41,6 +74,37 @@ export default function FrontOffice({
                     </NavLink>
                 </nav>
             </header>
+
+            <nav
+                id="mobilemenu"
+                className="flex flex-col"
+                style={{ display: hamburgerOpen ? 'block' : 'none' }}
+            >
+                <NavLink
+                    href={route('dashboard')}
+                    active={route().current('contract')}
+                >
+                    Nos Contrats
+                </NavLink>
+                <NavLink
+                    href={route('dashboard')}
+                    active={route().current('farmers')}
+                >
+                    Nos Producteurs
+                </NavLink>
+                <NavLink
+                    href={route('dashboard')}
+                    active={route().current('calendar')}
+                >
+                    Calendrier des saisons
+                </NavLink>
+                <NavLink
+                    href={route('dashboard')}
+                    active={route().current('contact')}
+                >
+                    Contact
+                </NavLink>
+            </nav>
             <div
                 className="flex h-[340px] items-end bg-stone-900 bg-cover bg-center p-4"
                 style={{ backgroundImage: `url(${image})` }}
--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -30,7 +30,7 @@ export default function Home() {
             }
             image={helpField}
         >
-            <section className="m-20 flex flex-col items-center">
+            <section className="mx-4 my-20 flex flex-col items-center md:mx-8">
                 <h2 className="my-3 text-2xl font-bold">
                     LES PRODUITS PROPOSÉS
                 </h2>
@@ -38,7 +38,7 @@ export default function Home() {
                     <Swiper
                         className="products"
                         navigation={true}
-                        slidesPerView={5}
+                        slidesPerView={2}
                         spaceBetween={30}
                         modules={[Autoplay, Navigation]}
                         loop={true}
@@ -133,7 +133,7 @@ export default function Home() {
                     </Swiper>
                 </div>
             </section>
-            <section className="m-20 grid grid-cols-[2fr,1fr]">
+            <section className="mx-4 my-20 md:mx-8 md:grid md:grid-cols-[2fr,1fr]">
                 <article className="mr-4">
                     <h2 className="my-3 text-2xl font-bold uppercase">
                         Une association au plus proche des producteurs
@@ -152,16 +152,22 @@ export default function Home() {
                         consequat quam porta habitasse. Nisl et ut sit nam.
                     </p>
                 </article>
-                <img className="h-full w-full rounded-lg" src={nearFarmers} />
+                <img
+                    className="h-full w-full rounded-lg max-md:hidden"
+                    src={nearFarmers}
+                />
             </section>
             <section className="my-20 flex flex-col items-center bg-zinc-800 p-5">
                 <h2 className="my-4 text-2xl font-bold uppercase dark:text-gray-200">
                     Nous sommes ici !
                 </h2>
-                <img src={localisation} className="my-2 h-[55vh]" />
+                <img src={localisation} className="my-2 h-[55vh] w-auto" />
             </section>
-            <section className="m-20 grid grid-cols-[1fr,2fr]">
-                <img className="h-full w-full rounded-lg" src={localFarmers} />
+            <section className="mx-4 my-20 md:mx-8 md:grid md:grid-cols-[1fr,2fr]">
+                <img
+                    className="h-full w-full rounded-lg max-md:hidden"
+                    src={localFarmers}
+                />
                 <article className="ml-4">
                     <h2 className="my-3 text-2xl font-bold uppercase">
                         Des producteurs locaux
@@ -181,7 +187,7 @@ export default function Home() {
                     </p>
                 </article>
             </section>
-            <section className="mx-8 mb-8 mt-20">
+            <section className="mx-4 mb-8 mt-20 md:mx-8">
                 <div className="h-full w-full rounded-lg bg-black p-5">
                     <h2 className="my-4 text-center text-2xl font-bold uppercase dark:text-gray-200">
                         Une convivialité qui rapproche
@@ -235,12 +241,12 @@ export default function Home() {
                     </Swiper>
                 </div>
             </section>
-            <p className="text-center">
+            <p className="mx-4 text-center md:mx-8">
                 Lorem ipsum dolor sit amet consectetur. Tristique in egestas
-                nisi vitae curabitur amet egestas eleifend volutpat. Eget
-                donec non pulvinar proin tristique lectus aliquet velit
-                aliquam. Vulputate arcu magna tortor fusce consequat quam
-                porta habitasse. Nisl et ut sit nam.
+                nisi vitae curabitur amet egestas eleifend volutpat. Eget donec
+                non pulvinar proin tristique lectus aliquet velit aliquam.
+                Vulputate arcu magna tortor fusce consequat quam porta
+                habitasse. Nisl et ut sit nam.
             </p>
         </FrontOffice>
     );
