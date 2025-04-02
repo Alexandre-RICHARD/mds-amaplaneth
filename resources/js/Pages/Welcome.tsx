978c0a9b0ccaf189519ee530f69742cd9c9588ee
Alexandre RICHARD <arichard@predell.fr>
2025-04-02 11:46:30 +0200

    test1

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -174,12 +174,7 @@ export default function Welcome({
                                         </h2>
 
                                         <p className="mt-4 text-sm/relaxed">
-                                            Laracasts offers thousands of video
-                                            tutorials on Laravel, PHP, and
-                                            JavaScript development. Check them
-                                            out, see for yourself, and massively
-                                            level up your development skills in
-                                            the process.
+                                            NOPEEEE
                                         </p>
                                     </div>
 
