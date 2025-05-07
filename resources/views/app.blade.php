93a38195dfd5c594f2cfc94321782f8b55cafb95
Alexandre Richard <alexandre.richard.dev@gmail.com>
2025-05-07 10:22:02 +0200

    Change favicon and app title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,11 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>Amap L'aneth</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
 
         <!-- Scripts -->
         @routes
