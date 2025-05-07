cfdd5258e9916f58352839dbf500bb58175030d8
Mahora Grolleau <hiphopfraise@gmail.com>
2025-05-07 15:02:40 +0200

    all migrations

---
+++ database/migrations/2025_05_07_123749_create_producer_products_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('producer_products', function (Blueprint $table) {
+            $table->foreignId('id_producer');
+            $table->foreignId('id_product');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('producer_products');
+    }
+};
