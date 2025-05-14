fc3a220db1db3d6463f4bfb0a13a1b725d47fcce
Mahora Grolleau <hiphopfraise@gmail.com>
2025-05-14 09:13:28 +0200

    All models

---
+++ app/Models/Producer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Producer extends Model
+{
+    use HasFactory;
+
+    protected $table = "producers";
+
+    protected $fillable = [
+        'profile_picture',
+        'first_name',
+        'last_name',
+        'address_road',
+        'zipcode',
+        'city',
+        'description'
+    ];
+}
