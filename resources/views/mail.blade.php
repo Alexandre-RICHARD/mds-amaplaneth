b6e4cb42ae007cc1970767eac835b98b5296cf32
Alexandre Richard <alexandre.richard.dev@gmail.com>
2025-06-23 01:20:12 +0200

    styling mail

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,1 +1,17 @@
-{{ $content }}
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <style>
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+            line-height: 1.5;
+        }
+    </style>
+</head>
+<body>
+    <div>
+        {!! nl2br(e($content)) !!}
+    </div>
+</body>
+</html>
